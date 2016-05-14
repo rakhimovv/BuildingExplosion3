b0c1536e9a737ebd318e7bf4b9fad902e5ac13bd
Ruslan <risonyo@gmail.com>
2016-05-14 04:16:15 +0300

    Changed Explosion physics

--- src/GameObjects/Explosion.cpp
+++ src/GameObjects/Explosion.cpp
@@ -26,17 +26,19 @@ void Explosion::Update(float dt) {
     }
 
     // Обработка взрыва
-    float currRadius = ((totalLifetime - elapsedLifetime) / totalLifetime) * this->maxRadius;
+    //float currRadius = ((totalLifetime - elapsedLifetime) / totalLifetime) * this->maxRadius;
 
     for (int i = 0; i < owner->GetParticleSystem()->GetParticlesCount(); i++) {
         ParticleHandle<ParticleInfo> blockParticle = owner->GetParticleSystem()->GetParticle(size_t(i));
 
         Vector3f distance = blockParticle.GetPos() - this->pos;
-        if (distance.Length() <= currRadius) {
-            float offset = 0.3f;
-            Vector3f resultAcceleration = blockParticle.GetAcceleration() + offset * distance.GetNorm();
-            blockParticle.SetAcceleration(resultAcceleration);
-        }
+        //if (distance.Length() <= currRadius) {
+        float force = 1.0f;
+        float a = distance.Length();
+        float offset = force / (a * a * a);
+        Vector3f resultAcceleration = blockParticle.GetAcceleration() + offset * distance.GetNorm();
+        blockParticle.SetAcceleration(resultAcceleration);
+        //}
     }
 }
 
